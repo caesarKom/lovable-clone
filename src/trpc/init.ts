2099067c918d53e71429bcac231b924ef42b8938
caesarKom <cezarykomorowski@gmail.com>
2025-07-05 16:56:33 +0200

    trpc setup

---
+++ src/trpc/init.ts
@@ -0,0 +1,17 @@
+import { initTRPC } from "@trpc/server";
+import { cache } from "react";
+import superjson from "superjson";
+
+export const createTRPCContext = cache(async () => {});
+
+const t = initTRPC.create({
+  /**
+   * @see https://trpc.io/docs/server/data-transformers
+   */
+  transformer: superjson,
+});
+
+// Base router and procedure helpers
+export const createTRPCRouter = t.router;
+export const createCallerFactory = t.createCallerFactory;
+export const baseProcedure = t.procedure;
