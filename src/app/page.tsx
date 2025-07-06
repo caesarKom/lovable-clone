ce763e4a6075ef35b679ce8fb38ee356c159961c
caesarKom <cezarykomorowski@gmail.com>
2025-07-06 20:03:59 +0200

    projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export default function HomePage() {
+  const router = useRouter();
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
@@ -17,8 +19,12 @@ export default function HomePage() {
       onError: (err) => {
         toast.error(err.message);
       },
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`);
+      },
     })
   );
+
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
