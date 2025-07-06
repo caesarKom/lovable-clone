e6f16f9714f5923dbfb1155d970ee972ad047987
caesarKom <cezarykomorowski@gmail.com>
2025-07-06 19:56:31 +0200

    projects

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -175,6 +175,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await db.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "Something went wrong, Please try agein.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -184,6 +185,7 @@ export const codeAgentFunction = inngest.createFunction(
 
       return await db.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
