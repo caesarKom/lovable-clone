07155359f4412331282bda1f4c1bd706b6cc75d5
caesarKom <cezarykomorowski@gmail.com>
2025-07-06 17:04:01 +0200

    agent tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
+
 export default function HomePage() {
-  return <div>Hello app</div>;
+  const [value, setValue] = useState("");
+
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+
+      <Button onClick={() => invoke.mutate({ value: value })}>
+        Invoke Background jobs
+      </Button>
+
+      <div className="p-4">{value}</div>
+    </div>
+  );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,37 @@
-import { createTRPCRouter } from "../init";
+import z from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(
+      z.object({
+        value: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          value: input.value,
+        },
+      });
+
+      return { ok: "Success" };
+    }),
+
+  createAI: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
